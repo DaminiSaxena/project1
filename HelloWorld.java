122017758af83a8d282f28766b3ec2505f106684
DaminiSaxena <daminisaxena2@gmail.com>
2019-01-28 19:07:33 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld{
 	public static void main(String args[])
 	{
-		System.out.println("Hello World from Master Branch!");
+		System.out.println("Hello World from Master Branch!")
 		System.exit(0);
 	}
 }
